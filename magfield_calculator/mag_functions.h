93a296bac427291acd735a1e9d566d58ecf66981
Pedro Manuel Quintela Ribeiro <pedro_rib2@hotmail.com>
2019-10-17 15:12:45 +0100

    Separated calculator from main code. Now allows for the simulation of multiple cilia

---
+++ magfield_calculator/mag_functions.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "pch.h"
+#include "veclib.h"
+#include <string>
+#include <iostream>
+#include <time.h>
+#include <thread>
+
+#define PARALLEL 1
+#define PARALLEL 1
+#define PI 3.14159265358979323846
+
+#ifndef MAGCAL
+#define MAGCAL
+
+void magCalc(vec *calculatedField, vec **destination, vec *magDipoles, vec *posDipoles, vec *dipoleDisplacement, double dim, int inputSize, unsigned int gridSize, unsigned int numPillars);
+
+void magCalc(vec *calculatedField, vec **destination, vec *magDipoles, vec *posDipoles, double dim, int inputSize, unsigned int gridSize);
+
+#endif
